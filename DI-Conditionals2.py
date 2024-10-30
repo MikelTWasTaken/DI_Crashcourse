a724faf8b4bfb7d948fb4bd85e9a4cd225b015d4
MikelTWasTaken <mike.b.teitelbaum@gmail.com>
2024-10-30 10:41:51 +0200

    Initial commit

---
+++ DI-Conditionals2.py
@@ -0,0 +1,38 @@
+"""
+l1 = [1,2,3,4,5]
+l2 = [1,2,3,4]
+
+if l1 == l2:
+    print (True)
+else:
+    print (False)
+
+a = 1
+b = 2
+
+if b is a:
+    print (True)
+else:
+    print (False)
+"""
+
+'''
+input('What do you want for lunch?')
+
+String = 'word'
+
+User = 'Elie'
+if User == 'Elie':
+    print ('Awesome')
+elif User == 'Tom':
+    print ('cool!')
+else:
+    print ('Nope')
+
+my_list = ["a", 1, True]
+
+'''
+
+values = [1,2,3,4]
+for val in values:
+    print (val)
